5ab20b3cc6ca273c9cb6d52fa2fab2f2417c149b
rahul <vashisth.rahul13@gmail.com>
2025-05-13 00:53:02 +0530

    Complete Revamping of Folder structure for streamlit app deployment

--- pages/4_Data_Insights.py
+++ pages/4_Data_Insights.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
-with open('../1_Datasets/sweetviz_report.html','r',encoding='utf-8') as html_file:
+with open('1_Datasets/sweetviz_report.html','r',encoding='utf-8') as html_file:
     html_content = html_file.read()
 
 st.title("Sweetviz Overall Data Insights")
